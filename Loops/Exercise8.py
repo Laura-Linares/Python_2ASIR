4a01a666c1d2556512c6c0f11f4592e4ca1eca7f
Laura-Linares <lauralinarescontacto@gmail.com>
2025-10-23 09:52:44 +0200

    exercise 8 - loops - done

---
+++ Loops/Exercise8.py
@@ -0,0 +1,21 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla
+#un triángulo rectángulo como el de más abajo.
+#1
+#3 1
+#5 3 1
+#7 5 3 1
+#9 7 5 3 1ç
+
+import os
+os.system("clear")
+
+num=int(input("Por favor, introduzca un número entero positivo: "))
+
+if num < 0:
+    print(f"El número {num} no es un entero positivo")
+else:
+    acumulador=1
+    for n in range(1,num+1,2):
+        for m in range(n,0,-2):
+            print(m, end=" ")
+        print("")
