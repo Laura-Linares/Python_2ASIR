f72003306179068a9ba169ad6b5f3a353217945d
Laura-Linares <lauralinarescontacto@gmail.com>
2025-10-30 11:14:14 +0100

    exercise 12 - lists and tuples - started

---
+++ Lists_and_tuples/Exercise12.py
@@ -0,0 +1,21 @@
+#Escribir un programa que almacene las matrices en una lista y muestre por pantalla su producto.
+#Nota: Para representar matrices mediante listas usar listas anidadas, representando
+#cada vector fila en una lista.
+
+import os
+os.system("clear")
+
+matriz=[
+    [2,2,2,2]
+    [2,2,2,2]
+    [2,2,2,2]
+    [2,2,2,2]
+]
+
+matriz2=[
+    [2,2,2,2]
+    [2,2,2,2]
+    [2,2,2,2]
+    [2,2,2,2]
+]
+
